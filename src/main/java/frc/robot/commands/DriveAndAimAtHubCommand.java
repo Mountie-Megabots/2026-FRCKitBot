cef3d7cf3059f871fc37dcb9e015bdb2dafad53b
Kevin Furlong <kevinmfurlong@gmail.com>
2026-01-20 05:19:31 -0800

    Added scaffold for driving and aiming.

---
+++ src/main/java/frc/robot/commands/DriveAndAimAtHubCommand.java
@@ -0,0 +1,147 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+/**
+ * This command lets the driver move the robot while
+ * the robot automatically turns to face the hub.
+ *
+ * STUDENT TASK:
+ * You will fill in the missing pieces so the robot:
+ *  - Figures out which alliance it is on
+ *  - Finds the hub on the field
+ *  - Calculates the angle to the hub
+ *  - Drives while facing the hub
+ */
+public class DriveAndAimAtHubCommand extends Command {
+
+    // The drivetrain subsystem
+    private final CommandSwerveDrivetrain drivetrain;
+
+    // Joystick inputs (forward/back and left/right)
+    private final DoubleSupplier xSpeed;
+    private final DoubleSupplier ySpeed;
+
+    // Which alliance are we on? (Red or Blue)
+    private DriverStation.Alliance alliance;
+
+    // The position of the hub on the field
+    private Pose2d hubPosition;
+
+    // These values change depending on alliance
+    private double allianceOffsetDeg;
+    private double allianceInversionFactor;
+
+    // This is the CTRE swerve request we send every loop
+    private final SwerveRequest.FieldCentricFacingAngle drive =
+            new SwerveRequest.FieldCentricFacingAngle()
+                    .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+                    .withSteerRequestType(SteerRequestType.MotionMagicExpo);
+
+    public DriveAndAimAtHubCommand(
+            CommandSwerveDrivetrain drivetrain,
+            DoubleSupplier xSpeed,
+            DoubleSupplier ySpeed
+    ) {
+        this.drivetrain = drivetrain;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+
+        // Tell WPILib that this command uses the drivetrain
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+
+        /*
+         * TODO 1: Figure out which alliance we are on
+         *
+         * Hint:
+         *  - Use DriverStation.getAlliance()
+         *  - If it does not exist, default to Blue
+         */
+
+        // alliance = ???
+
+        /*
+         * TODO 2: Based on the alliance:
+         *  - Set hubPosition
+         *  - Set allianceOffsetDeg
+         *  - Set allianceInversionFactor
+         *
+         * Hints:
+         *  - Blue and Red hubs are in Constants.FieldConstants
+         *  - Red alliance usually needs a 180 degree offset
+         *  - One alliance needs its joystick directions flipped
+         */
+    }
+
+    @Override
+    public void execute() {
+
+        /*
+         * TODO 3: Get the robot's current position on the field
+         *
+         * Hint:
+         *  - drivetrain.getState().Pose
+         */
+        Pose2d robotPose = null;
+
+        /*
+         * TODO 4: Find the X and Y distance from the robot to the hub
+         *
+         * Hint:
+         *  - dx = hubX - robotX
+         *  - dy = hubY - robotY
+         */
+        double dx = 0;
+        double dy = 0;
+
+        /*
+         * TODO 5: Use atan2 to calculate the angle to the hub (in radians)
+         *
+         * Hint:
+         *  - Math.atan2(y, x)
+         */
+        double angleToHubRad = 0;
+
+        /*
+         * TODO 6: Create a Translation2d from joystick inputs
+         *
+         * Steps:
+         *  - Read xSpeed and ySpeed
+         *  - Multiply by max speed
+         *  - Apply alliance inversion
+         *  - Rotate by the robot's current rotation
+         */
+        Translation2d translation = new Translation2d();
+
+        /*
+         * TODO 7: Send the command to the drivetrain
+         *
+         * You must:
+         *  - Set velocity X
+         *  - Set velocity Y
+         *  - Set the target direction (angle to hub + alliance offset)
+         */
+        drivetrain.setControl(
+                drive
+                    // .withVelocityX( ??? )
+                    // .withVelocityY( ??? )
+                    // .withTargetDirection( ??? )
+        );
+    }
+}
